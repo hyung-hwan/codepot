42bf53b7739ba5f66490c6aa0744d92f997bfd5c
hyung-hwan <hyunghwan.chung@gmail.com>
2010-05-17 13:11:42 +0000

    * fixed a bug in getFile() of the subversion model class * enhanced the code folder view

--- codepot/src/codepot/language/english/code_lang.php
+++ codepot/src/codepot/language/english/code_lang.php
@@ -3,5 +3,6 @@ $lang['CODE_COMMIT'] = 'Commit';
 $lang['CODE_PROPERTIES'] = 'Properties';
 $lang['CODE_PROPERTY'] = 'Property';
 
+$lang['CODE_MSG_COMMITTED_BY'] = 'Committed by %s';
 $lang['CODE_MSG_COMMITTED_BY_ON'] = 'Committed by %s on %s';
 ?>
