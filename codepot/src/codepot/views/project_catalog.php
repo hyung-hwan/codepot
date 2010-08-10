2ffc4574f6d989a5d45c74c8ebc8e2b46e5b93b4
hyunghwan.chung <hyunghwan.chung@acb928ee-5b15-407b-8704-3b33df051210>
2010-08-10 07:31:12 +0000

    deleted a redundant line from project_catalog.php
    git-svn-id: https://code.abiyo.net/svn/codepot/trunk@58 acb928ee-5b15-407b-8704-3b33df051210

--- codepot/src/codepot/views/project_catalog.php
+++ codepot/src/codepot/views/project_catalog.php
@@ -2,7 +2,6 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="keywords" content="<?=$project->id?>" />
 
 <link type="text/css" rel="stylesheet" href="<?=base_url_make('/css/common.css')?>" />
 <link type="text/css" rel="stylesheet" href="<?=base_url_make('/css/project.css')?>" />
