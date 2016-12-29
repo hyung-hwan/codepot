9ce4d4710bf599c48df706973ee4d63ae0b6223a
hyunghwan.chung <hyunghwan.chung@gmail.com>
2016-12-29 15:26:38 +0000

    changed layout of the code history view. removed graphs from the view too
    git-svn-id: file:///var/lib/codepot/svnrepo/codepot/trunk@558 acb928ee-5b15-407b-8704-3b33df051210

--- codepot/src/codepot/views/code_history.php
+++ codepot/src/codepot/views/code_history.php
@@ -92,8 +92,10 @@ $this->load->view (
 	<div style="clear: both;"></div>
 </div>
 
-<div class="graph" id="code_history_graph">
+
+<!-- <div class="graph" id="code_history_graph">
 <?php
+/*
 	$xfullpath = $this->converter->AsciiToHex (($fullpath == '')? '.': $fullpath);
 
 	$graph_url = codepot_merge_path (site_url(), "/code/graph/commits-by-users/{$project->id}/{$xfullpath}{$revreq}");
@@ -101,18 +103,16 @@ $this->load->view (
 
 	$graph_url = codepot_merge_path (site_url(), "/code/graph/commit-share-by-users/{$project->id}/{$xfullpath}{$revreq}");
 	print "<img src='{$graph_url}' />";
+*/
 ?>
-</div> <!-- code_history_graph -->
+</div>  -->
 
 
 <div id="code_history_result" class="codepot-relative-container-view">
 	<table id="code_history_result_table" class="codepot-full-width-table codepot-spacious-table">
 	<tr class='codepot-full-width-table-header'>
-		<th><?php /*print $this->lang->line('Revision')*/ ?></th>
 		<th><?php print $this->lang->line('Committer')?></th>
-		<th><?php print $this->lang->line('Date')?></th>
-		<th><?php print $this->lang->line('Message')?></th>
-		<?php if ($file['type'] == 'file' || $file['type'] == 'dir') print '<th></th>'; ?>
+		<th><?php print $this->lang->line('Details') ?></th>
 	</tr>
 	<?php 
 		$rowclasses = array ('even', 'odd');
@@ -122,40 +122,31 @@ $this->load->view (
 		for ($i = $history_count; $i > 0; )
 		{
 			$h = $history[--$i];
+			$xfullpath = $this->converter->AsciiToHex (($fullpath == '')? '.': $fullpath);
 
 			$rowclass = $rowclasses[($history_count - $i) % 2];
-			print "<tr class='{$rowclass}'>";
+			//print "<tr class='{$rowclass}'>";
+			print "<tr>";
 
-			print '<td class="commit-revision-td">';
-			$xfullpath = $this->converter->AsciiToHex (
-				($fullpath == '')? '.': $fullpath);
-
-			print anchor ("code/file/{$project->id}/{$xfullpath}/{$h['rev']}", $h['rev']);
-
-			if (!empty($h['tag']))
-			{
-				print ' ';
-				print '<span class="left_arrow_indicator">';
-				print htmlspecialchars($h['tag']);
-				print '</span>';
-			}
-			print '</td>';
-
-			print '<td class="commit-author-td">';
+			print '<td class="commit-author-td" rowspan=2>';
 			// Repository migration from googlecode revealed that it did not put 
 			// 'author' for initial project creation. So I've added the following check.
 			if (array_key_exists('author', $h)) 
 			{
 				$user_icon_url = codepot_merge_path (site_url(), '/user/icon/' . $this->converter->AsciiToHex($h['author']));
-				print "<img src='{$user_icon_url}' class='codepot-committer-icon-24x24' /> ";
+				$user_home_url = codepot_merge_path (site_url(), '/user/home/' . $this->converter->AsciiToHex($h['author']));
+				//print "<img src='{$user_icon_url}' class='codepot-committer-icon-24x24' /> ";
+				print "<div class='commit-author-icon'>";
+				print "<a href='{$user_home_url}'><img src='{$user_icon_url}' class='commit-author-icon' /></a>";
+				print "</div>";
 
+				print '<div class="commit-author-id">';
+				print "<a href='{$user_home_url}'>";
 				print htmlspecialchars($h['author']);
+				print '</a>';
+				print '</div>';
 			}
 			print '</td>';
-
-			print '<td class="commit-date-td"><tt>';
-			print codepot_unixtimetodispdate(strtotime($h['date']), 'Y-m-d');
-			print '</tt></td>';
 
 			print '<td class="commit-message-td">';
 			if ($h['review_count'] > 0)
@@ -169,18 +160,43 @@ $this->load->view (
 			//print htmlspecialchars($h['msg']);
 			//print '</pre>';
 			print '</td>';
+			print '</tr>';
 
-			print '<td>';
+			print '<tr>';
+			print '<td class="commit-revision-td">';
+
+			print '<div class="commit-revision-and-datetime">';
+			print '<div class="commit-revision">';
+			print '<tt>';
+			print anchor ("code/file/{$project->id}/{$xfullpath}/{$h['rev']}", $h['rev']);
+			if (!empty($h['tag']))
+			{
+				print ' ';
+				print '<span class="left_arrow_indicator">';
+				print htmlspecialchars($h['tag']);
+				print '</span>';
+			}
+			print '</tt>';
+			print '</div>';
+
+			print '<div class="commit-datetime">';
+			print '<tt>';
+			print codepot_unixtimetodispdate(strtotime($h['date']));
+			print '</tt>';
+			print '</div>';
+			print '</div>';
+
+			print '<div class="commit-actions">';
 			if ($file['type'] == 'file')
 			{
 				print anchor ("code/blame/{$project->id}/{$xfullpath}/{$h['rev']}", 
-					'<i class="fa fa-bomb"></i> ' . $this->lang->line('Blame'));
-				print ' | ';
+					'<div class="commit-action-item"><i class="fa fa-bomb"></i> ' . $this->lang->line('Blame') . '</div>');
+				
 				print anchor ("code/diff/{$project->id}/{$xfullpath}/{$h['rev']}",
-					'<i class="fa fa-server"></i> ' . $this->lang->line('Difference')); 
+					'<div class="commit-action-item"><i class="fa fa-server"></i> ' . $this->lang->line('Difference') . '</div>'); 
 			}
+			print '</div>';
 			print '</td>';
-
 			print '</tr>';
 
 			//
