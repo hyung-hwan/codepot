3316cf8b3fc7fb73f08b680338935c90d33bcd56
hyung-hwan <hyunghwan.chung@gmail.com>
2019-02-01 15:23:22 +0000

    enhanced AccessHandler.pm for httpd 2.4

--- codepot/etc/perl/Codepot/AuthenHandler.pm
+++ codepot/etc/perl/Codepot/AuthenHandler.pm
@@ -3,10 +3,17 @@ package Codepot::AuthenHandler;
 use strict;
 use warnings;
 
-use Apache2::Const -compile => qw(OK DECLINED FORBIDDEN HTTP_UNAUTHORIZED  HTTP_INTERNAL_SERVER_ERROR);
+use Apache2::Access ();
+use Apache2::RequestUtil ();
+use Apache2::RequestRec ();
+use Apache2::Log;
 
+use Apache2::Const -compile => qw(OK DECLINED FORBIDDEN HTTP_UNAUTHORIZED  HTTP_INTERNAL_SERVER_ERROR AUTH_REQUIRED);
+
+use Data::Dumper;
 sub handler: method
 {
+	my ($class, $r) = @_;
 	return Apache2::Const::OK;
 }
 1;
